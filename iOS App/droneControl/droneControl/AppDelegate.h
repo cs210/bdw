46858042771fd1f50dab52ebd3ae939596223806
Michael Weingert <mpweinge@uwaterloo.ca>
2015-03-08 16:50:55 -0700

    Initial Commit

---
+++ iOS App/droneControl/droneControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  droneControl
+//
+//  Created by Michael Weingert on 2015-03-08.
+//  Copyright (c) 2015 bdw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
