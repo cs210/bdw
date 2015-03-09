8cd201dc99a5aaa3c02194b9ea03557b9c60300d
Ellen Sebastian <ellen.sebastian@healthtap.com>
2015-03-09 01:53:03 -0700

    update AppDelegate to include BMW boilerplate.
    Bug in didFinishLaunchingWithOptions. commented out a line.

--- iOS App/droneControl/droneControl/AppDelegate.h
+++ iOS App/droneControl/droneControl/AppDelegate.h
@@ -7,11 +7,18 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "RootViewController.h"
+
+
+@class RemoteApplicationManager;
+@class IDAccessoryMonitor;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
+@property (strong) RemoteApplicationManager *manager;
+@property (strong) IDAccessoryMonitor *accessoryMonitor;
 
 @end
 
--- iOS App/droneControl/droneControl/AppDelegate.m
+++ iOS App/droneControl/droneControl/AppDelegate.m
@@ -1,45 +1,178 @@
-//
-//  AppDelegate.m
-//  droneControl
-//
-//  Created by Michael Weingert on 2015-03-08.
-//  Copyright (c) 2015 bdw. All rights reserved.
-//
-
 #import "AppDelegate.h"
+#import <ExternalAccessory/ExternalAccessory.h>
+#import <BMWAppKit/BMWAppKit.h>
+#import "RemoteApplicationManager.h"
+#import "IDNSLoggerAppender.h"
+#import "LastUserModeDelegate.h"
 
-@interface AppDelegate ()
+
+@interface AppDelegate () <LastUserModeDelegate>
+
+@property (strong) id a4aAccessoryDidConnectObserver;
+@property (strong) id a4aAccessoryDidDisconnectObserver;
+@property (strong) id a4aAccessoryMonitorNetworkAccessObserver;
+@property (assign) BOOL focusAfterStartingInRemoteHmi;
+@property (strong) IDNSLoggerAppender *nsLoggerAppender;
 
 @end
 
 @implementation AppDelegate
 
+static NSString *const RemoteApplicationStateKeyPath = @"remoteApplicationState";
+static NSString *const LoggerHostBonjourServiceNameKeyPath = @"logger_host_bonjour_service_name";
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  // Override point for customization after application launch.
-  return YES;
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    [application setIdleTimerDisabled:YES];
+    self.focusAfterStartingInRemoteHmi = NO;
+    
+    // enable BMWAppKit log output
+    IDLogger *logger = [IDLogger defaultLogger];
+    [logger setMaximumLogLevel:IDLogLevelDebug];
+    
+    IDConsoleLogAppender *consoleAppender = [IDConsoleLogAppender new];
+    [logger addAppender:consoleAppender];
+    [consoleAppender setMaximumLogLevel:IDLogLevelWarn];
+    
+    NSString *bonjourHostName = [[NSUserDefaults standardUserDefaults] valueForKeyPath:LoggerHostBonjourServiceNameKeyPath];
+    
+    self.nsLoggerAppender = [[IDNSLoggerAppender alloc] initWithBonjourHostName:bonjourHostName];
+    [self.nsLoggerAppender setMaximumLogLevel:IDLogLevelDebug];
+    [logger addAppender:self.nsLoggerAppender];
+    
+    // register for EAF notifications to enable BMWAppKit to receive notifications from BMW accessories
+    [[EAAccessoryManager sharedAccessoryManager] registerForLocalNotifications];
+    
+    // register for BMWAppKit specific notifications
+    self.manager = [RemoteApplicationManager new];
+    self.manager.lumDelegate = self;
+    NSKeyValueObservingOptions options = NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew;
+    //[self.manager addObserver:self forKeyPath:RemoteApplicationStateKeyPath options:options context:nil];
+    // above line causes an "unrecognized selector sent to instance" error.
+    [self startMonitoringA4ACompatibleAccessories];
+    
+    return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-  // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-  // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    NSString *bonjourHostName = [[NSUserDefaults standardUserDefaults] valueForKeyPath:LoggerHostBonjourServiceNameKeyPath];
+    self.nsLoggerAppender.bonjourHostName = bonjourHostName;
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-  // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-  // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    [application setIdleTimerDisabled:NO];
+    [self.manager removeObserver:self forKeyPath:RemoteApplicationStateKeyPath context:nil];
+    
+    // deregister from EAF notifications
+    [[EAAccessoryManager sharedAccessoryManager] unregisterForLocalNotifications];
+    [self stopMonitoringA4ACompatibleAccessories];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-  // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
+    if ([[url path] rangeOfString:@"helloworld"].location == NSNotFound)
+    {
+        return NO;
+    }
+    
+    for (NSDictionary *dict in [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleURLTypes"]) {
+        if ([[dict objectForKey:@"CFBundleURLSchemes"] containsObject:[url scheme]] )
+        {
+            self.focusAfterStartingInRemoteHmi = YES;
+            return YES;
+        }
+    }
+    return NO;
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-  // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+#pragma mark - LastUserModeDelegate override
+
+- (BOOL)shouldFocusInRemoteHmi
+{
+    BOOL focus = self.focusAfterStartingInRemoteHmi;
+    self.focusAfterStartingInRemoteHmi = NO;
+    return focus;
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application {
-  // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+#pragma mark - Helper methods
+
+- (void)startMonitoringA4ACompatibleAccessories
+{
+    __weak id weakSelf = self;
+    
+    // register for BMW specific notifications
+    self.a4aAccessoryMonitorNetworkAccessObserver = [[NSNotificationCenter defaultCenter] addObserverForName:IDAccessoryNetworkAccessDidChangeNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note) {
+        BOOL isUsingNetwork = [note.userInfo[IDAccessoryUsingNetworkKey] boolValue];
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:isUsingNetwork];
+    }];
+    
+    self.a4aAccessoryDidConnectObserver = [[NSNotificationCenter defaultCenter] addObserverForName:IDAccessoryDidConnectNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notification) {
+        AppDelegate *appDelegate = (AppDelegate *)weakSelf;
+        [[appDelegate rootViewController] updateConnectionState:ConnectionStateConnectedToVehicle];
+        [appDelegate.manager startRemoteApplication];
+    }];
+    
+    self.a4aAccessoryDidDisconnectObserver = [[NSNotificationCenter defaultCenter] addObserverForName:IDAccessoryDidDisconnectNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notification) {
+        AppDelegate *appDelegate = (AppDelegate *)weakSelf;
+        [appDelegate.manager stopRemoteApplication];
+        [[appDelegate rootViewController] updateConnectionState:ConnectionStateNotConnectedToVehicle];
+    }];
+    
+    // tell the accessory monitor to start monitoring and retain it
+    self.accessoryMonitor = [IDAccessoryMonitor new];
+    [self.accessoryMonitor startMonitoring];
+}
+
+- (void)stopMonitoringA4ACompatibleAccessories
+{
+    [self.accessoryMonitor stopMonitoring];
+    self.accessoryMonitor = nil;
+    
+    if (self.a4aAccessoryDidConnectObserver)
+    {
+        [[NSNotificationCenter defaultCenter] removeObserver:self.a4aAccessoryDidConnectObserver];
+        self.a4aAccessoryDidConnectObserver = nil;
+    }
+    
+    if (self.a4aAccessoryDidDisconnectObserver)
+    {
+        [[NSNotificationCenter defaultCenter] removeObserver:self.a4aAccessoryDidDisconnectObserver];
+        self.a4aAccessoryDidDisconnectObserver = nil;
+    }
+}
+
+#pragma mark - KVO
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if ([keyPath isEqualToString:RemoteApplicationStateKeyPath] && [object isEqual:self.manager])
+    {
+        RemoteApplicationState state = [[change valueForKey:NSKeyValueChangeNewKey] intValue];
+        [[self rootViewController] updateRemoteApplicationState:state];
+    }
+    else
+    {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+#pragma mark - Helper methods
+
+- (RootViewController *)rootViewController
+{
+    UINavigationController *navigationController = (UINavigationController *)self.window.rootViewController;
+    UIViewController *viewController = navigationController.viewControllers[0];
+    
+    if ([viewController isKindOfClass:[RootViewController class]])
+    {
+        return (RootViewController *)viewController;
+    }
+    
+    return nil;
 }
 
 @end
