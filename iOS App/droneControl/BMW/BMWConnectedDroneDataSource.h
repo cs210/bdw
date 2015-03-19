5e6886cc038a46a701872d2ba6d8c2b0c80df23b
Ellen Sebastian <ellen.sebastian@healthtap.com>
2015-03-19 07:30:02 -0700

    project cleanup: move all frameworks into Frameworks

---
+++ iOS App/droneControl/BMW/BMWConnectedDroneDataSource.h
@@ -0,0 +1,13 @@
+//
+//  BMWConnectedDroneDataSource.h
+//  droneControl
+//
+//  Created by Ellen Sebastian on 3/19/15.
+//  Copyright (c) 2015 bdw. All rights reserved.
+//
+
+#ifndef droneControl_BMWConnectedDroneDataSource_h
+#define droneControl_BMWConnectedDroneDataSource_h
+
+
+#endif
---
+++ iOS App/droneControl/BMW/BMWConnectedDroneDataSource.m
@@ -0,0 +1,9 @@
+//
+//  BMWConnectedDroneDataSource.m
+//  droneControl
+//
+//  Created by Ellen Sebastian on 3/19/15.
+//  Copyright (c) 2015 bdw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
