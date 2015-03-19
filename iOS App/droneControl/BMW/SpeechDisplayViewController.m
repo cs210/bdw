8a776de63f46ce9b09e62f4f9c7dc79affc48d2f
Ellen Sebastian <ellen.sebastian@healthtap.com>
2015-03-19 04:56:11 -0700

    changed WaitingViewController to a mapView which tracks the location of the drone and syncs up with HMI correctly.

--- iOS App/droneControl/BMW/SpeechDisplayViewController.m
+++ iOS App/droneControl/BMW/SpeechDisplayViewController.m
@@ -3,6 +3,7 @@
 #import "HelloWorldDataSource.h"
 #import "SpeechController.h"
 #import "SimulatedNavigationViewController.h"
+#import "WaitingViewController.h"
 
 typedef enum
 {
@@ -82,7 +83,7 @@ typedef enum
     _lastHeardWord.text = word;
     if ([word isEqualToString:@"FIND PARKING"]){
         //For now transition to other view controller
-        [self.navigationController pushViewController:[[SimulatedNavigationViewController alloc] init] animated:NO];
+        [self.navigationController pushViewController:[[WaitingViewController alloc] init] animated:NO];
     }
     
 }
--- iOS App/droneControl/droneControl/WaitingViewController.m
+++ iOS App/droneControl/droneControl/WaitingViewController.m
@@ -8,20 +8,97 @@
 
 /*This is going to be a map interface*/
 #import "WaitingViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface WaitingViewController () {}
+@interface WaitingViewController () < CLLocationManagerDelegate>
+{
+    MKMapView * _mapView;
+    CLLocationManager *_locationManager;
+    CLLocation * _userLocation;
+}
+
+@property (nonatomic, strong) MKPointAnnotation *point;
+@property (nonatomic) int n_times_moved;
+@property (nonatomic, strong) NSTimer *timer;
+
 
 @end
 
 @implementation WaitingViewController
 
+- (void) moveAnnotation{
+    if (_n_times_moved > 10){
+        [_timer invalidate];
+        return;
+    }
+    [_mapView removeAnnotation:_point];
+    
+    CLLocationCoordinate2D fakeMapPoint;
+    fakeMapPoint.longitude = _userLocation.coordinate.longitude + (0.0001 * (float) _n_times_moved);
+    fakeMapPoint.latitude = _userLocation.coordinate.latitude + (0.0001 * (float) _n_times_moved);
+    
+    _point.coordinate = fakeMapPoint;
+    _point.title = @"Drone";
+    //_point.subtitle = @"Drone";
+    [_mapView addAnnotation:_point];
+    [_mapView selectAnnotation:_point animated:YES];
+    _n_times_moved += 1;
+    NSLog(@"n_times_moved: %d",_n_times_moved);
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    UILabel *waitingLabel = [[UILabel alloc]initWithFrame:CGRectMake(100,100,100,100)];
-    waitingLabel.text = @"Drone is flying and finding a parking space...";
+    _n_times_moved = 0;
+    _mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:_mapView];
+    _mapView.showsUserLocation = YES;
+    
+    //Get the users current location
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+    [_locationManager requestAlwaysAuthorization];
+    [_locationManager startUpdatingLocation];
+    
+    CLLocationCoordinate2D noLocation;
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 500, 500);
+    MKCoordinateRegion adjustedRegion = [_mapView regionThatFits:viewRegion];
+    [_mapView setRegion:adjustedRegion animated:YES];
 }
 
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    if (_n_times_moved == 0){
+        CLLocation *crnLoc = [locations lastObject];
+        _userLocation = crnLoc;
+        
+        MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(_userLocation.coordinate, 500, 500);
+        MKCoordinateRegion adjustedRegion = [_mapView regionThatFits:viewRegion];
+        [_mapView setRegion:adjustedRegion animated:YES];
+        
+        // Add an annotation
+        
+        _timer = [NSTimer scheduledTimerWithTimeInterval:1
+                                                  target:self
+                                                selector:@selector(moveAnnotation)
+                                                userInfo:nil
+                                                 repeats:YES];
+        _point = [[MKPointAnnotation alloc] init];
+        
+        CLLocationCoordinate2D fakeMapPoint;
+        fakeMapPoint.longitude = _userLocation.coordinate.longitude ;
+        fakeMapPoint.latitude = _userLocation.coordinate.latitude;
+        
+        _point.coordinate = fakeMapPoint;
+        _point.title = @"Drone";
+        //_point.subtitle = @"Drone";
+        [_mapView addAnnotation:_point];
+        [_mapView selectAnnotation:_point animated:NO];
+        _n_times_moved = 1;
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
