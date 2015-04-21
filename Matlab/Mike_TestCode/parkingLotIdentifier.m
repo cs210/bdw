4c5f5c1b97b4f40f3706f46914585febc81f1c58
Michael Weingert <mpweinge@uwaterloo.ca>
2015-04-20 20:59:43 -0700

    Second pass, Mike's Speckle Filter

--- Matlab/Mike_TestCode/parkingLotIdentifier.m
+++ Matlab/Mike_TestCode/parkingLotIdentifier.m
@@ -5,29 +5,31 @@ grayImage = rgb2gray(im);
 [rows, columns, numberOfColorChannels] = size(grayImage);
 
 im1 = grayImage(rows / 2 : rows, columns / 2 : columns, 1:numberOfColorChannels);
+im1 = imresize(im1, [400 NaN]);
 imshow(im1)
 
 gaussFilter = fspecial('gaussian')
 
-figure;
-imshow(imfilter(im1, gaussFilter, 'replicate'))
+%figure;
+%imshow(imfilter(im1, gaussFilter, 'replicate'))
 
-figure;
-hold on;
-Canny1 = edge(im1, 'Canny');
-Sobel1 = edge(im1, 'Sobel');
+%figure;
+%hold on;
+%Canny1 = edge(im1, 'Canny');
+%Sobel1 = edge(im1, 'Sobel');
 
-subplot(1, 3, 1)
-imshow(Canny1)
-title('Canny')
+%subplot(1, 3, 1)
+%imshow(Canny1)
+%title('Canny')
 
-subplot(1,3,2)
-imshow(Sobel1)
-title('Sobel')
+%subplot(1,3,2)
+%imshow(Sobel1)
+%title('Sobel')
 
 thresholdedImage = im1;
-for row = 1 : rows / 2
-    for column = 1 : columns / 2
+[newRows, newColumns, ~] = size(im1);
+for row = 1 : newRows
+    for column = 1 : newColumns
         if (im1(row,column) > 160)
             thresholdedImage(row, column) = 255;
         else
@@ -35,9 +37,9 @@ for row = 1 : rows / 2
         end
     end
 end
-subplot(1,3,3)
-imshow(thresholdedImage)
-title('Thresholded Image')
+%subplot(1,3,3)
+%imshow(thresholdedImage)
+%title('Thresholded Image')
 figure;
 imshow(thresholdedImage)
 
@@ -46,4 +48,6 @@ imshow(thresholdedImage)
 %neighboring pixels, and start "exploring" in that detection. With the
 %thresholding, speckels will disapear as the number of neighboring pixels
 %is small
-mikeSpeckleFilter(thresholdedImage, 3)
+newImage = mikeSpeckleFilter(thresholdedImage, 3);
+figure
+imshow(newImage);
