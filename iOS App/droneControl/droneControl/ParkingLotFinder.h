2e24571c4ed2081ba49dea6d0dfb36f85a87e9fb
Ellen Sebastian <ellen.sebastian@healthtap.com>
2015-04-24 00:10:56 -0700

    add ParkingLotFinder api

--- iOS App/droneControl/droneControl/ParkingLotFinder.h
+++ iOS App/droneControl/droneControl/ParkingLotFinder.h
@@ -7,6 +7,17 @@
 //
 // this file finds parking lots around a certain location.
 
+
+struct ParkingLot{
+    CLLocationCoordinate2D coordinate;
+    CLLocationCoordinate2D upperLeft; // upperLeft and lowerRight represent the area occupied by the parking lot.
+    CLLocationCoordinate2D lowerRight;
+    NSString* name;
+};
+
 @interface ParkingLotFinder : NSObject
+
+// should be called off the main thread as it needs to wait for http results. 
+-(NSArray) parkingLotsNearby: (CLLocationCoordinate2D) userLocation (Integer) radius;
 
 @end
