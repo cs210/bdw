6b9d8efec71f4ee0e96b519d43a299f5feb4ed8a
Michael Weingert <mpweinge@uwaterloo.ca>
2015-04-26 19:55:47 -0700

    Clicking codez

--- Matlab/Mike_TestCode/A1_q3_ginput.m
+++ Matlab/Mike_TestCode/A1_q3_ginput.m
@@ -10,16 +10,24 @@ title('Click a point on this image'); axis image;
 %Take in corner input
 
 ImageOneCorners = zeros(2, 20);
+[rows, columns, ~] = size(img1);
+hold on
+plot(columns / 2, rows / 2,'r*')
+
+columns / 2
+rows / 2
 
 %Photo taken from a height of 12 cm.
-for i = 1:20
+for i = 1:200
     [x, y] = ginput(1);
     %your points are now stored as [x,y]
-    fprintf(['You clicked at: ' num2str(x) ', ' num2str(y) '\n']);
+    fprintf([num2str(x) ', ' num2str(y) '\n']);
     ImageOneCorners(i, 1)= x;
     ImageOneCorners(i, 2) = y;
     img1(round(y),round(x)) = 255;
     imagesc(img1); colormap(gray);
+    hold on
+    plot(x,y,'ro')
     title('Click a point on this image'); axis image;
 end
 
