60a3632864b071512d857c30452e74a64f7e7f74
Michael Weingert <mpweinge@uwaterloo.ca>
2015-04-28 00:12:50 -0700

    Splitview controller

--- iOS App/droneControl/droneControl/NearbyParkingTableViewController.m
+++ iOS App/droneControl/droneControl/NearbyParkingTableViewController.m
@@ -22,6 +22,9 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+  
+  self.tableView.dataSource = self;
+  self.tableView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,24 +37,27 @@
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return 2;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TEST" forIndexPath:indexPath];
     
     // Configure the cell...
+  
+  //cell.text = @"LOL";
+  cell.textLabel.text = @"LOL";
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
