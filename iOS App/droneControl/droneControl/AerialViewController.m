8055405bb321905a9c49c7fa56b111d1efe9b475
Michael Weingert <mpweinge@uwaterloo.ca>
2015-05-31 23:42:25 -0700

    Automatically recenter view when user location moves

--- iOS App/droneControl/droneControl/AerialViewController.m
+++ iOS App/droneControl/droneControl/AerialViewController.m
@@ -197,6 +197,11 @@
     
     [self.view addSubview:_googleMapView];
     
+    [_googleMapView addObserver:self
+               forKeyPath:@"myLocation"
+                  options:NSKeyValueObservingOptionNew
+                  context:NULL];
+    
     dispatch_async(dispatch_get_main_queue(), ^{
         _googleMapView.myLocationEnabled = YES;
     });
@@ -232,6 +237,20 @@
 
 -(void) viewWillAppear:(BOOL)animated{
    // [self launchDrone];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context {
+    //if (!_firstLocationUpdate) {
+        // If the first location update has not yet been recieved, then jump to that
+        // location.
+    //    _firstLocationUpdate = YES;
+        CLLocation *location = [change objectForKey:NSKeyValueChangeNewKey];
+        _googleMapView.camera = [GMSCameraPosition cameraWithTarget:location.coordinate
+                                        zoom:14];
+    //}
 }
 
 
