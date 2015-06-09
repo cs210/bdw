7030ca881874399655a01e7d8f78779df72a9353
Michael Weingert <mpweinge@uwaterloo.ca>
2015-06-09 02:29:48 -0700

    Speech Controller documentation

--- iOS App/droneControl/droneControl/SpeechController.h
+++ iOS App/droneControl/droneControl/SpeechController.h
@@ -12,19 +12,42 @@
 
 @protocol SpeechDelegate
 
+/*!
+ * @discussion Return a list of words we want to listen for
+ * @return NSArray The list of words we want to listen for
+ */
 -(NSArray * ) listOfWordsToDetect;
+
+/*!
+ * @discussion Protocol function that gets called when a word is identified
+ * @param word The word that was identified
+ */
 -(void) didReceiveWord: (NSString *) word;
 
 @end
 
 @interface SpeechController : NSObject < OEEventsObserverDelegate >
 
+/*!
+ * @discussion Init the speech controller with a listener delegate
+ * @param delegate The delegate that will receive speech events
+ * @return The SpeechController object
+ */
 -(instancetype) initWithDelegate: (id<SpeechDelegate>)delegate;
 
+/*!
+ * @discussion Function to initialize the speech handler
+ */
 -(void) setupSpeechHandler;
 
+/*!
+ * @discussion Function to start the speech handler listening
+ */
 -(void) startListening;
 
+/*!
+ * @discussion Function to stop the speech handler listening
+ */
 -(void) stopListening;
 
 @end
